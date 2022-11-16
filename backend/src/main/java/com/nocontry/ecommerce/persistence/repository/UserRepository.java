81e07b5fb85a2b134921e8efcec0ad99a987410b
pieromc07 <piero0716.mc@gmail.com>
2022-11-16 12:09:54 -0500

    agregando filtro de busqueda por username

--- backend/src/main/java/com/nocontry/ecommerce/persistence/repository/UserRepository.java
+++ backend/src/main/java/com/nocontry/ecommerce/persistence/repository/UserRepository.java
@@ -7,5 +7,7 @@ import com.nocontry.ecommerce.persistence.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
+    User findByEmail(String email);
+
 }
