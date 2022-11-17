e62ecd9def7dc4f908fcf8c6ab6254af580d8a20
pieromc07 <piero0716.mc@gmail.com>
2022-11-17 10:56:46 -0500

    creacion del endpoint login

--- backend/src/main/java/com/nocontry/ecommerce/mapper/impl/UserMapper.java
+++ backend/src/main/java/com/nocontry/ecommerce/mapper/impl/UserMapper.java
@@ -3,7 +3,9 @@ package com.nocontry.ecommerce.mapper.impl;
 import com.nocontry.ecommerce.mapper.IUserMapper;
 import com.nocontry.ecommerce.persistence.model.AccountState;
 import com.nocontry.ecommerce.persistence.model.User;
+import com.nocontry.ecommerce.rest.dto.request.LoginRequest;
 import com.nocontry.ecommerce.rest.dto.request.RegisterRequest;
+import com.nocontry.ecommerce.rest.dto.response.LoginResponse;
 import com.nocontry.ecommerce.rest.dto.response.RegisterResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -37,6 +39,22 @@ public class UserMapper implements IUserMapper {
         user.setStatus(AccountState.ACTIVE);
 
         return user;
+    }
+
+    @Override
+    public User mapLoginEntity(LoginRequest loginRequest) {
+        return User.builder()
+                .username(loginRequest.getEmail())
+                .password(passwordEncoder.encode(loginRequest.getPassword()))
+                .build();
+    }
+
+    @Override
+    public LoginResponse toLoginResponse(User user) {
+        return LoginResponse.builder()
+              .username(user.getUsername())
+              .email(user.getEmail()).token("token")
+              .build();
     }
 
 }
