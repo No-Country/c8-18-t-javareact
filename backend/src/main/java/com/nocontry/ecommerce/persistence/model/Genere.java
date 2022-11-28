cc3d5aa21e64d24ee0de45d8018bfab06ae84858
Carlos Piero Merino Carpio <piero0716.mc@gmail.com>
2022-11-28 15:14:30 -0500

    :construction_worker: crecion de model y repository de genero

---
+++ backend/src/main/java/com/nocontry/ecommerce/persistence/model/Genere.java
@@ -0,0 +1,26 @@
+package com.nocontry.ecommerce.persistence.model;
+
+import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "generes")
+public class Genere {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "genere_id")
+    private Long genereId;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+}
