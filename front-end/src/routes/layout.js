21725fbb884c4402ba1b95002ff6b4bc47d54cdb
Israel <israelg1058@gmail.com>
2022-11-28 12:31:12 -0500

    page in home

--- front-end/src/routes/layout.js
+++ front-end/src/routes/layout.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
-import '../App.css';
+import { Link } from 'react-router-dom';
 import { BiUser } from 'react-icons/bi';
-import { BsCart4 } from 'react-icons/bs';
+import { MdMarkEmailRead, MdLocationPin } from 'react-icons/md';
+import { BsWhatsapp, BsFacebook, BsCart4 } from 'react-icons/bs';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+
+import Navbar from '../components/Navbar';
+
+import '../App.css';
 
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 const Layout = ({ children }) => {
   const [open, setOpen] = useState(false);
@@ -28,11 +33,31 @@ const Layout = ({ children }) => {
         </div>
       </div>
       <div>
-      --Nav Aqui
+        <Navbar/>
       </div>
       <main>{children}</main>
-      <footer>
-        --Footer Aqui
+      <footer className='grid md:grid-cols-3 grid-cols-1 mt-8 px-8'>
+        <div className='py-3'>
+          <h2 className='font-semibold text-2xl'>Sobre Nosotros</h2>
+          <p className='w-80 pt-3'>Lorem ipsum dolor sit amet consectetur. Gravida mauris vel lacus quis dis
+            congue urna mus. Eu ut maecenas pellentesque enim.</p>
+        </div>
+        <div className='md:mx-auto mx-0 py-3'>
+          <h2 className='font-semibold text-2xl'>Contactos</h2>
+          <ul className='pt-3'>
+            <li className='flex pt-3'><MdMarkEmailRead className='text-2xl'/><span className='ml-3'>email@example.com</span></li>
+            <li className='flex py-3'><MdLocationPin className='text-2xl'/><span className='ml-3'>Location Example</span></li>
+            <li className='flex'><BsWhatsapp className='text-2xl'/><span className='ml-3'>09999999999</span></li>
+          </ul>
+        </div>
+        <div className='md:mx-auto mx-0 py-3'>
+          <h2 className='font-semibold text-2xl'>Redes</h2>
+          <ul className='pt-3 flex'>
+            <li><BsFacebook className='text-[2.5rem] mr-4'/></li>
+            <li><AiFillInstagram className='text-5xl mx-4'/></li>
+            <li><AiOutlineTwitter className='text-5xl'/></li>
+          </ul>
+        </div>
       </footer>
     </div>
   );
